1aa918c02d16c9c3bdedaaf58f2e95d7a4d7e448
Stephen Smithwick <stephen.smithwick@gmail.com>
2013-04-14 00:25:16 +1000

    Instead of raising an exception, why not use method_missing as a convenient way to call the defined methods

--- lib/multiblock.rb
+++ lib/multiblock.rb
@@ -8,7 +8,7 @@ class Multiblock < BlankSlate
   end
 
   def method_missing(name, *args, &blk)
-    raise ArgumentError.new("No block given when registering '#{name}' block.") unless block_given?
+    return call(name, *args) unless block_given?
     @blocks[name.to_s] = blk
   end
 
