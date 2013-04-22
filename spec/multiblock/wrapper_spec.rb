f61c08507633d7a05695c925a5bfd812e951025d
Michał Szajbe <michal.szajbe@gmail.com>
2013-04-22 22:03:54 +0200

    Changed variable naming from multiblock to wrapper

--- spec/multiblock/wrapper_spec.rb
+++ spec/multiblock/wrapper_spec.rb
@@ -2,46 +2,46 @@ require "spec_helper"
 
 describe Multiblock::Wrapper do
 
-  let(:multiblock) {
+  let(:wrapper) {
     described_class.new
   }
 
   it "should register block" do
-    multiblock.foo { "foo" }
-    multiblock.call(:foo).should == "foo"
+    wrapper.foo { "foo" }
+    wrapper.call(:foo).should == "foo"
   end
 
   it "should call registered block by String" do
-    multiblock.foo { "foo" }
-    multiblock.call("foo").should == "foo"
+    wrapper.foo { "foo" }
+    wrapper.call("foo").should == "foo"
   end
 
   it "should return nil when calling unregistered block" do
-    multiblock.call(:foo).should be_nil
+    wrapper.call(:foo).should be_nil
   end
 
   it "should return nil when calling unregistered block with arguments" do
-    multiblock.call(:foo).should be_nil
+    wrapper.call(:foo).should be_nil
   end
 
   it "should pass arguments to called block" do
-    multiblock.foo { |arg| arg }
-    multiblock.call(:foo, "foo").should == "foo"
+    wrapper.foo { |arg| arg }
+    wrapper.call(:foo, "foo").should == "foo"
   end
 
   it "should raise ArgumentError exception when registering without block" do
     lambda {
-      multiblock.foo
+      wrapper.foo
     }.should raise_exception(ArgumentError, "No block given when registering 'foo' block.")
   end
 
   context "with custom default block" do
-    let(:multiblock) {
+    let(:wrapper) {
       described_class.new { "default" }
     }
 
     it "should call custom default block when calling unregistered block" do
-      multiblock.call(:foo).should == "default"
+      wrapper.call(:foo).should == "default"
     end
   end
 end
